f4ec1436e17530d7f76923cc458fc7213d9b6829
AzzyPog <renato.longo1406@gmail.com>
2024-01-16 00:21:25 -0300

    [init] iniciando o ambiente dev
    ambiente dev iniciado com as dependências presentes.

---
+++ src/main/java/br/ufrj/backendsiga/BackendSigaApplication.java
@@ -0,0 +1,13 @@
+package br.ufrj.backendsiga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSigaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSigaApplication.class, args);
+	}
+
+}
