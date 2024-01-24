5847358eb34c484c339f2d7f2b9d99d7f908e29b
jotapecds <jpcoelhodesouza@gmail.com>
2024-01-24 13:48:30 -0300

    [Feature] Listagem de ICs disponíveis para aluno - Nova rota GET no controller de IC: findAllIniciacaoCientifica(); - Nova rota GET no controller de IC: getIniciacaoCientificaById(icId). [equipesiga/treinamento-dev-2024/boardtreinamentodev#3]

--- src/main/java/br/ufrj/backendsiga/controller/IniciacaoCientificaController.java
+++ src/main/java/br/ufrj/backendsiga/controller/IniciacaoCientificaController.java
@@ -1,8 +1,11 @@
 package br.ufrj.backendsiga.controller;
 
 import br.ufrj.backendsiga.model.entity.IniciacaoCientifica;
+import br.ufrj.backendsiga.repository.IniciacaoCientificaRepository;
 import br.ufrj.backendsiga.service.IniciacaoCientificaService;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,8 +16,21 @@ import java.util.List;
 @RestController
 @RequestMapping("/ic")
 @RequiredArgsConstructor
+@CrossOrigin
 public class IniciacaoCientificaController {
+
+    private final IniciacaoCientificaRepository iniciacaoCientificaRepository;
     private final IniciacaoCientificaService iniciacaoCientificaService;
+
+    @GetMapping()
+    public List<IniciacaoCientifica> findAllIniciacaoCientifica() {
+        return iniciacaoCientificaRepository.findAll();
+    }
+
+    @GetMapping("/{icId}")
+    public IniciacaoCientifica getIniciacaoCientificaById(@PathVariable String icId) {
+        return iniciacaoCientificaService.getIniciacaoCientificaById(Integer.parseInt(icId));
+    }
 
     @GetMapping("/professor/{matricula}")
     public List<IniciacaoCientifica> listIniciacaoCientificaProfessor(@PathVariable String matricula) {
--- src/main/java/br/ufrj/backendsiga/service/IniciacaoCientificaService.java
+++ src/main/java/br/ufrj/backendsiga/service/IniciacaoCientificaService.java
@@ -5,9 +5,13 @@ import br.ufrj.backendsiga.model.entity.Usuario;
 import br.ufrj.backendsiga.repository.IniciacaoCientificaRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -15,6 +19,19 @@ public class IniciacaoCientificaService {
     private final IniciacaoCientificaRepository iniciacaoCientificaRepository;
     private final UsuarioService usuarioService;
     private final CargoService cargoService;
+
+    public IniciacaoCientifica getIniciacaoCientificaById(Integer icId) {
+        Optional<IniciacaoCientifica> optIc = iniciacaoCientificaRepository.findById(icId);
+
+        if (optIc.isEmpty()) {
+            throw new ResponseStatusException(
+                HttpStatus.NOT_FOUND, 
+                "Iniciação científica inexistente."
+            );
+        }
+
+        return optIc.get();
+    }
 
     @Transactional
     public List<IniciacaoCientifica> listIniciacaoCientificaByProfessorMatricula(String matricula) {
