5b6944fa9638fea4d906bc6b52c6612e8a1b49c1
jotapecds <jpcoelhodesouza@gmail.com>
2024-01-29 11:41:39 -0300

    [Feature] Filtro de ICs para aluno - Criada nova rota para listar todos os professores; - Criação do UsuarioMapper.. [equipesiga/treinamento-dev-2024/boardtreinamentodev#7]

---
+++ src/main/java/br/ufrj/backendsiga/model/mapping/UsuarioMapper.java
@@ -0,0 +1,13 @@
+package br.ufrj.backendsiga.model.mapping;
+
+import br.ufrj.backendsiga.model.dto.UsuarioDTO;
+import br.ufrj.backendsiga.model.entity.Usuario;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface UsuarioMapper {
+    UsuarioMapper INSTANCE = Mappers.getMapper(UsuarioMapper.class);
+
+    UsuarioDTO toDTO(Usuario usuario);
+}
